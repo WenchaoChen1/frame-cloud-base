ac3e057a9e1b3b41e3f48ef453a7276f44230d73
wenchao <wenchao@gstdev.com>
2024-03-25 17:42:11 +0800

    delete annotation

--- gstdev-cloud-oauth2/gstdev-cloud-oauth2-sdk-authentication/src/main/java/com/gstdev/cloud/oauth2/authentication/provider/AbstractAuthenticationProvider.java
+++ gstdev-cloud-oauth2/gstdev-cloud-oauth2-sdk-authentication/src/main/java/com/gstdev/cloud/oauth2/authentication/provider/AbstractAuthenticationProvider.java
@@ -1,39 +1,19 @@
 package com.gstdev.cloud.oauth2.authentication.provider;
 
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.session.SessionInformation;
 import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClaimAccessor;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.OAuth2Error;
-import org.springframework.security.oauth2.core.OAuth2RefreshToken;
-import org.springframework.security.oauth2.core.OAuth2Token;
+import org.springframework.security.oauth2.core.*;
 import org.springframework.security.oauth2.core.OAuth2AccessToken.TokenType;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
-import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.OAuth2Authorization.Token;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AccessTokenAuthenticationToken;
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
@@ -41,6 +21,11 @@ import org.springframework.security.oauth2.server.authorization.token.DefaultOAu
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
 import org.springframework.util.CollectionUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
 
 public abstract class AbstractAuthenticationProvider implements AuthenticationProvider {
   private final Log logger = LogFactory.getLog(this.getClass());
@@ -56,7 +41,7 @@ public abstract class AbstractAuthenticationProvider implements AuthenticationPr
   }
 
   protected OAuth2AccessToken createOAuth2AccessToken(DefaultOAuth2TokenContext.Builder tokenContextBuilder, OAuth2Authorization.Builder authorizationBuilder, OAuth2TokenGenerator<? extends OAuth2Token> tokenGenerator, String errorUri) {
-    OAuth2TokenContext tokenContext = ((DefaultOAuth2TokenContext.Builder)tokenContextBuilder.tokenType(OAuth2TokenType.ACCESS_TOKEN)).build();
+    OAuth2TokenContext tokenContext = tokenContextBuilder.tokenType(OAuth2TokenType.ACCESS_TOKEN).build();
     OAuth2Token generatedAccessToken = tokenGenerator.generate(tokenContext);
     if (generatedAccessToken == null) {
       OAuth2Error error = new OAuth2Error("server_error", "The token generator failed to generate the access token.", errorUri);
@@ -69,7 +54,7 @@ public abstract class AbstractAuthenticationProvider implements AuthenticationPr
       OAuth2AccessToken accessToken = new OAuth2AccessToken(TokenType.BEARER, generatedAccessToken.getTokenValue(), generatedAccessToken.getIssuedAt(), generatedAccessToken.getExpiresAt(), tokenContext.getAuthorizedScopes());
       if (generatedAccessToken instanceof ClaimAccessor) {
         authorizationBuilder.token(accessToken, (metadata) -> {
-          metadata.put(Token.CLAIMS_METADATA_NAME, ((ClaimAccessor)generatedAccessToken).getClaims());
+          metadata.put(Token.CLAIMS_METADATA_NAME, ((ClaimAccessor) generatedAccessToken).getClaims());
         });
       } else {
         authorizationBuilder.accessToken(accessToken);
@@ -82,7 +67,7 @@ public abstract class AbstractAuthenticationProvider implements AuthenticationPr
   protected OAuth2RefreshToken creatOAuth2RefreshToken(DefaultOAuth2TokenContext.Builder tokenContextBuilder, OAuth2Authorization.Builder authorizationBuilder, OAuth2TokenGenerator<? extends OAuth2Token> tokenGenerator, String errorUri, OAuth2ClientAuthenticationToken clientPrincipal, RegisteredClient registeredClient) {
     OAuth2RefreshToken refreshToken = null;
     if (registeredClient.getAuthorizationGrantTypes().contains(AuthorizationGrantType.REFRESH_TOKEN) && !clientPrincipal.getClientAuthenticationMethod().equals(ClientAuthenticationMethod.NONE)) {
-      OAuth2TokenContext tokenContext = ((DefaultOAuth2TokenContext.Builder)tokenContextBuilder.tokenType(OAuth2TokenType.REFRESH_TOKEN)).build();
+      OAuth2TokenContext tokenContext = tokenContextBuilder.tokenType(OAuth2TokenType.REFRESH_TOKEN).build();
       OAuth2Token generatedRefreshToken = tokenGenerator.generate(tokenContext);
       if (!(generatedRefreshToken instanceof OAuth2RefreshToken)) {
         OAuth2Error error = new OAuth2Error("server_error", "The token generator failed to generate the refresh token.", errorUri);
@@ -93,7 +78,7 @@ public abstract class AbstractAuthenticationProvider implements AuthenticationPr
         this.logger.trace("Generated refresh token");
       }
 
-      refreshToken = (OAuth2RefreshToken)generatedRefreshToken;
+      refreshToken = (OAuth2RefreshToken) generatedRefreshToken;
       authorizationBuilder.refreshToken(refreshToken);
     }
 
@@ -115,7 +100,7 @@ public abstract class AbstractAuthenticationProvider implements AuthenticationPr
         tokenContextBuilder.put(SessionInformation.class, sessionInformation);
       }
 
-      OAuth2TokenContext tokenContext = ((DefaultOAuth2TokenContext.Builder)((DefaultOAuth2TokenContext.Builder)tokenContextBuilder.tokenType(ID_TOKEN_TOKEN_TYPE)).authorization(authorizationBuilder.build())).build();
+      OAuth2TokenContext tokenContext = tokenContextBuilder.tokenType(ID_TOKEN_TOKEN_TYPE).authorization(authorizationBuilder.build()).build();
       OAuth2Token generatedIdToken = tokenGenerator.generate(tokenContext);
       if (!(generatedIdToken instanceof Jwt)) {
         OAuth2Error error = new OAuth2Error("server_error", "The token generator failed to generate the ID token.", errorUri);
@@ -126,7 +111,7 @@ public abstract class AbstractAuthenticationProvider implements AuthenticationPr
         this.logger.trace("Generated id token");
       }
 
-      idToken = new OidcIdToken(generatedIdToken.getTokenValue(), generatedIdToken.getIssuedAt(), generatedIdToken.getExpiresAt(), ((Jwt)generatedIdToken).getClaims());
+      idToken = new OidcIdToken(generatedIdToken.getTokenValue(), generatedIdToken.getIssuedAt(), generatedIdToken.getExpiresAt(), ((Jwt) generatedIdToken).getClaims());
       authorizationBuilder.token(idToken, (metadata) -> {
         metadata.put(Token.CLAIMS_METADATA_NAME, idToken.getClaims());
       });
@@ -142,12 +127,12 @@ public abstract class AbstractAuthenticationProvider implements AuthenticationPr
     if (sessionRegistry != null) {
       List<SessionInformation> sessions = sessionRegistry.getAllSessions(principal.getPrincipal(), false);
       if (!CollectionUtils.isEmpty(sessions)) {
-        sessionInformation = (SessionInformation)sessions.get(0);
+        sessionInformation = sessions.get(0);
         if (sessions.size() > 1) {
 //          List<SessionInformation> sessions = new ArrayList(sessions);
-           sessions = new ArrayList(sessions);
+          sessions = new ArrayList(sessions);
           sessions.sort(Comparator.comparing(SessionInformation::getLastRequest));
-          sessionInformation = (SessionInformation)sessions.get(sessions.size() - 1);
+          sessionInformation = sessions.get(sessions.size() - 1);
         }
       }
     }
@@ -159,10 +144,10 @@ public abstract class AbstractAuthenticationProvider implements AuthenticationPr
     Map<String, Object> additionalParameters = Collections.emptyMap();
     if (idToken != null) {
       additionalParameters = new HashMap();
-      ((Map)additionalParameters).put("id_token", idToken.getTokenValue());
+      additionalParameters.put("id_token", idToken.getTokenValue());
     }
 
-    return (Map)additionalParameters;
+    return additionalParameters;
   }
 
   protected Set<String> validateScopes(Set<String> requestedScopes, RegisteredClient registeredClient) {
@@ -170,8 +155,8 @@ public abstract class AbstractAuthenticationProvider implements AuthenticationPr
     if (!CollectionUtils.isEmpty(requestedScopes)) {
       Iterator var4 = requestedScopes.iterator();
 
-      while(var4.hasNext()) {
-        String requestedScope = (String)var4.next();
+      while (var4.hasNext()) {
+        String requestedScope = (String) var4.next();
         if (!registeredClient.getScopes().contains(requestedScope)) {
           throw new OAuth2AuthenticationException("invalid_scope");
         }
@@ -180,7 +165,7 @@ public abstract class AbstractAuthenticationProvider implements AuthenticationPr
       authorizedScopes = new LinkedHashSet(requestedScopes);
     }
 
-    return (Set)authorizedScopes;
+    return authorizedScopes;
   }
 
   protected OAuth2AccessTokenAuthenticationToken createOAuth2AccessTokenAuthenticationToken(Authentication source, OAuth2AccessTokenAuthenticationToken destination) {
