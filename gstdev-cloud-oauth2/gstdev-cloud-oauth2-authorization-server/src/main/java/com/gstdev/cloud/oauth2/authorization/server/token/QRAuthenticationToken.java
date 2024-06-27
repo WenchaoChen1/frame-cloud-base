3e0cb5c3ce468e3973d3b32011b9eb4accaf2175
wenchao <wenchao@goldensection.com>
2024-06-27 18:03:18 +0800

    no message

--- gstdev-cloud-oauth2/gstdev-cloud-oauth2-authorization-server/src/main/java/com/gstdev/cloud/oauth2/authorization/server/token/QRAuthenticationToken.java
+++ gstdev-cloud-oauth2/gstdev-cloud-oauth2-authorization-server/src/main/java/com/gstdev/cloud/oauth2/authorization/server/token/QRAuthenticationToken.java
@@ -1,43 +1,43 @@
-// ====================================================
+//// ====================================================
+////
+//// This file is part of the GstDev Cloud Platform.
+////
+//// Create by GstDev Cloud <support@gstdev.com>
+//// Copyright (c) 2022-2025 gstdev.com
+////
+//// ====================================================
 //
-// This file is part of the GstDev Cloud Platform.
+//package com.gstdev.cloud.oauth2.authorization.server.token;
 //
-// Create by GstDev Cloud <support@gstdev.com>
-// Copyright (c) 2022-2025 gstdev.com
+//import lombok.Getter;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 //
-// ====================================================
-
-package com.gstdev.cloud.oauth2.authorization.server.token;
-
-import lombok.Getter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-
-import java.util.Collection;
-
-@Getter
-public class QRAuthenticationToken extends PreAuthenticatedAuthenticationToken {
-    private String type;
-    private String clientId;
-    private String code;
-
-
-    public QRAuthenticationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
-        super(principal, credentials, authorities);
-    }
-
-    public QRAuthenticationToken(Object principal, Object credentials) {
-        super(principal, credentials, null);
-        setAuthenticated(false);
-    }
-
-    public QRAuthenticationToken(String type, String clientId, String code) {
-        super(clientId, code, null);
-
-        this.type = type;
-        this.clientId = clientId;
-        this.code = code;
-
-        setAuthenticated(false);
-    }
-}
+//import java.util.Collection;
+//
+//@Getter
+//public class QRAuthenticationToken extends PreAuthenticatedAuthenticationToken {
+//    private String type;
+//    private String clientId;
+//    private String code;
+//
+//
+//    public QRAuthenticationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
+//        super(principal, credentials, authorities);
+//    }
+//
+//    public QRAuthenticationToken(Object principal, Object credentials) {
+//        super(principal, credentials, null);
+//        setAuthenticated(false);
+//    }
+//
+//    public QRAuthenticationToken(String type, String clientId, String code) {
+//        super(clientId, code, null);
+//
+//        this.type = type;
+//        this.clientId = clientId;
+//        this.code = code;
+//
+//        setAuthenticated(false);
+//    }
+//}
